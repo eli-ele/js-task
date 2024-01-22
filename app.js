082e7afb52b386d25104a40f4e035ee6fe8cca5a
eli-ele <150908647+eli-ele@users.noreply.github.com>
2024-01-22 21:25:07 +0400

    homework

---
+++ app.js
@@ -0,0 +1,51 @@
+// let num = -4;
+// if(num>0){
+//     console.log("es ricxvi dadebitia");
+// }else{
+//     console.log("es ricxvi uarkofitia");
+// }
+// function calculator(num1,num2,symbol){
+//     switch (symbol) {
+//         case "+":
+//             console.log(num1+num2);
+//             break;
+//         case"-":
+//             console.log(num1-num2);
+//             break;
+//         case"*":
+//             console.log(num1*num2);
+//             break;
+//         case"/":
+//             console.log(num1/num2);
+//             break;
+//         default:
+//             console.log("araa ricxvi")
+//             break;
+//     }
+// }
+// calculator(6,6,"+")
+
+// function calculator(num1, num2, simbol) {
+//    if (simbol==="+") {
+//     console.log(num1+num2);
+//    }else if(simbol==="-"){
+//     console.log(num1-num2);
+//    }else if(simbol==="*"){
+//     console.log(num1*num2);
+//    }else{
+//     console.log(num1/num2);
+//    }
+// }
+// calculator(3,4,"/");
+
+let firstName;
+
+let lastName;
+let email;
+let password;
+
+function firstName() {
+  if ((length = 0)) {
+    console.log("input is required");
+  } else console.log("daregistrirda warmatebit");
+}
